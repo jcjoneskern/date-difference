82d83f415536659b60da6c619c479099b1b83a2f
J-C <jcjoneskern@gmail.com>
2017-12-12 21:59:52 -0500

    solved

---
+++ date-difference/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace date_difference
+{
+    class DateDiff
+    {
+        public DateTime getDate()
+        {
+            Console.WriteLine("Enter a date (mm/dd/yyyy):");
+            String date = Console.ReadLine();
+
+            bool valid = DateTime.TryParseExact(date, "MM/dd/yyyy", new CultureInfo("en-US"), DateTimeStyles.None, out DateTime result);
+
+            while (!valid)
+            {
+                Console.WriteLine("That is not a valid date. Enter a date (mm/dd/yyyy):");
+                date = Console.ReadLine();
+                valid = DateTime.TryParseExact(date, "MM/dd/yyyy", new CultureInfo("en-US"), DateTimeStyles.None, out result);
+            }
+
+            DateTime parsedDate = DateTime.ParseExact(date, "MM/dd/yyyy", new CultureInfo("en-US"), DateTimeStyles.None);
+
+            return parsedDate;
+        }
+
+
+        static void Main(string[] args)
+        {
+            DateDiff d = new DateDiff();
+
+            DateTime date1 = d.getDate(), date2 = d.getDate();
+
+            TimeSpan dateRange = (date1 - date2);
+
+            double totalDays = dateRange.TotalDays, totalHours = dateRange.TotalHours, totalMinutes = dateRange.TotalMinutes;
+
+            Console.WriteLine("There are {0} days, or {1} hours, or {2} minutes between {3:d} and {4:d}", Math.Abs(totalDays), Math.Abs(totalHours), Math.Abs(totalMinutes), date1, date2);
+
+            Console.ReadKey();
+        }
+    }
+}
